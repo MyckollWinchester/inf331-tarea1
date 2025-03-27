16256a925dffa1d6337ab8eef167193a13e63be9
MyckollWinchester <myckoll.winchester@usm.cl>
2025-03-27 20:52:19 -0300

    Implement database setup and user management functions

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,10 +1,10 @@
 import bcrypt 
-  
-def hash_password(password): 
-    bytes = password.encode('utf-8') 
-    salt = bcrypt.gensalt() 
+
+def hash_password(password):
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
     return bcrypt.hashpw(bytes, salt)
 
-def verify_password(password, hashed): 
-    bytes = password.encode('utf-8') 
+def verify_password(password, hashed):
+    bytes = password.encode('utf-8')
     return bcrypt.checkpw(bytes, hashed)

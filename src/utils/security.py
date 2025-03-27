926c4e390cf9976608bf0fbc969ec6d2522af205
MyckollWinchester <myckoll.winchester@usm.cl>
2025-03-27 20:25:15 -0300

    Add password hashing and verification functions

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,1 +1,10 @@
-import bcrypt
+import bcrypt 
+  
+def hash_password(password): 
+    bytes = password.encode('utf-8') 
+    salt = bcrypt.gensalt() 
+    return bcrypt.hashpw(bytes, salt)
+
+def verify_password(password, hashed): 
+    bytes = password.encode('utf-8') 
+    return bcrypt.checkpw(bytes, hashed)

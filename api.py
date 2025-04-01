4112786af71aa40e255cca423f4e0a2926419327
EstebanCastilloTello <esteban.castillo@sansano.usm.cl>
2025-04-01 15:01:14 -0300

    JWT update in API

--- api.py
+++ api.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from src.blueprints.product import product_bp
 from src.blueprints.user import user_bp
 from src.database import close_db
 
 app = Flask(__name__)
 
+# Configuración de JWT
+app.config["JWT_SECRET_KEY"] = "super-secret-key"  # Cambia esto por una clave secreta segura
+jwt = JWTManager(app)
+
+# Registrar blueprints
 app.register_blueprint(product_bp)
 app.register_blueprint(user_bp, url_prefix='/api')
 
